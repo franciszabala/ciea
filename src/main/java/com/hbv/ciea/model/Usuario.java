d4700550e89234080c52ed75a7432367589f39c3
barrantesgerman <barrantesgerman@gmail.com>
2014-10-21 17:32:24 -0600

    Integración con Spring Boot Data Mongo y Spring Boot Data Rest

---
+++ src/main/java/com/hbv/ciea/model/Usuario.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2014 Codigo Fantasma.
+ *
+ * This work is licensed under the 
+ * Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+ * To view a copy of this license, visit 
+ *
+ *      http://creativecommons.org/licenses/by-nc-nd/3.0/
+ *
+ * or send a letter to Creative Commons, 444 Castro Street, Suite 900, 
+ * Mountain View, California, 94041, USA.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hbv.ciea.model;
+
+import org.springframework.data.annotation.Id;
+
+/**
+ *
+ * @author Herman Barrantes
+ * @since 21-oct-2014
+ */
+public class Usuario {
+
+    @Id
+    private String id;
+    private String nombre;
+    private String usuario;
+    private String clave;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String usuario, String clave) {
+        this.nombre = nombre;
+        this.usuario = usuario;
+        this.clave = clave;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+}
---
+++ src/main/java/com/hbv/ciea/repository/UsuarioRepository.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2014 Codigo Fantasma.
+ *
+ * This work is licensed under the 
+ * Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+ * To view a copy of this license, visit 
+ *
+ *      http://creativecommons.org/licenses/by-nc-nd/3.0/
+ *
+ * or send a letter to Creative Commons, 444 Castro Street, Suite 900, 
+ * Mountain View, California, 94041, USA.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hbv.ciea.repository;
+
+import com.hbv.ciea.model.Usuario;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+/**
+ *
+ * @author Herman Barrantes
+ * @since 21-oct-2014
+ */
+@RepositoryRestResource(collectionResourceRel = "usuarios", path = "usuario")
+public interface UsuarioRepository extends MongoRepository<Usuario, String> {
+
+    Usuario findByUsuario(@Param("usuario") String usuario);
+}
