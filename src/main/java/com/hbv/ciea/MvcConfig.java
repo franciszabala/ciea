a91ade1c1e3147d87fcd51a44c7d5218d3cda0a8
barrantesgerman <barrantesgerman@gmail.com>
2014-11-26 10:29:18 -0600

    cambio de idioma

--- src/main/java/com/hbv/ciea/MvcConfig.java
+++ src/main/java/com/hbv/ciea/MvcConfig.java
@@ -17,9 +17,15 @@
  */
 package com.hbv.ciea;
 
+import java.util.Locale;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 /**
  *
@@ -33,5 +39,27 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login/login");
+    }
+
+    /*
+     @see http://justinrodenbostel.com/2014/05/13/part-4-internationalization-in-spring-boot/
+     */
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.getDefault());
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
