ec8035c8d154ac30955d946134ec09363671cc07
barrantesgerman <barrantesgerman@gmail.com>
2014-11-26 11:35:55 -0600

    eliminando métodos no usados

--- src/main/java/com/hbv/ciea/Application.java
+++ src/main/java/com/hbv/ciea/Application.java
@@ -15,7 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.hbv.ciea;
 
 import com.hbv.ciea.model.Sitio;
@@ -33,42 +32,6 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-//        prueba();
-    }
-    
-    public static void prueba() {
-
-        ConfigurableApplicationContext context = SpringApplication.run(Application.class);
-        SitioRepository repository = context.getBean(SitioRepository.class);
-
-        // save a couple of customers
-        repository.save(new Sitio("Dirección"));
-        repository.save(new Sitio("Aula 1"));
-        repository.save(new Sitio("Aula 2"));
-        repository.save(new Sitio("Aula 3"));
-        repository.save(new Sitio("Aula 4"));
-        repository.save(new Sitio("Aula 5"));
-        repository.save(new Sitio("Aula 6"));
-        repository.save(new Sitio("Aula 7"));
-        repository.save(new Sitio("Aula 8"));
-
-        // fetch all customers
-        Iterable<Sitio> sitios = repository.findAll();
-        System.out.println("Sitios found with findAll():");
-        System.out.println("----------------------------");
-        for (Sitio sitio : sitios) {
-            System.out.println(sitio);
-        }
-        System.out.println();
-
-        // fetch an individual customer by ID
-        Sitio sitio = repository.findOne(1L);
-        System.out.println("Sitio found with findOne(1L):");
-        System.out.println("-----------------------------");
-        System.out.println(sitio);
-        System.out.println();
-
-        context.close();
     }
 
 }
