b9d774e6b3607d4863aef5695a3a865a5e21bd7d
Herman Barrantes <Herman Barrantes@10.1.5.189>
2015-01-21 09:00:33 -0600

    -Primeros pasos para usar XML

---
+++ src/main/java/com/hbv/ciea/dto/ListWrapper.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2015 Codigo Fantasma.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hbv.ciea.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAnyElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Herman Barrantes
+ * @since 21-ene-2015
+ */
+@XmlRootElement(name = "lista")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ListWrapper<T> {
+
+    @XmlAnyElement(lax = true)
+    private List<T> items;
+
+    public ListWrapper() {
+        items = new ArrayList<T>();
+    }
+
+    public ListWrapper(List<T> items) {
+        this.items = items;
+    }
+
+    public List<T> getItems() {
+        return items;
+    }
+
+}
