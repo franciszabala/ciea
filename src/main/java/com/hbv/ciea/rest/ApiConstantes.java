c896436a44e06342dfadc272a7af845243905275
Eilyn Salazar <eilyn.05@gmail.com>
2015-01-24 21:13:55 -0600

    Agregar mantenimiento de Activos

--- src/main/java/com/hbv/ciea/rest/ApiConstantes.java
+++ src/main/java/com/hbv/ciea/rest/ApiConstantes.java
@@ -34,5 +34,6 @@ public class ApiConstantes {
     public static final String API_PROVEEDOR = "/api/proveedor";
     public static final String API_USUARIO = "/api/usuario";
     public static final String API_PERFIL = "/api/perfil";
-    public static final String API_ARTICULO  = "/api/articulo";
+    public static final String API_ARTICULO = "/api/articulo";
+    public static final String API_ACTIVO = "/api/activo";
 }
