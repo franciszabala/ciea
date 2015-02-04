340d7e6caa79be06885964c969927c8505adf49c
Herman Barrantes <barrantesgerman@gmail.com>
2015-02-03 21:26:16 -0600

    -Pruebas reporte

--- src/main/java/com/hbv/ciea/controller/WelcomeController.java
+++ src/main/java/com/hbv/ciea/controller/WelcomeController.java
@@ -29,7 +29,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.jasperreports.JasperReportsHtmlView;
 import org.springframework.web.servlet.view.jasperreports.JasperReportsPdfView;
+import org.springframework.web.servlet.view.jasperreports.JasperReportsXlsView;
 
 /**
  *
