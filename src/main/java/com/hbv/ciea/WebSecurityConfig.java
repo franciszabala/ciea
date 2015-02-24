782b06b5d5162357b5ee13091b98857f6ed28dd3
Herman Barrantes <Herman Barrantes@GB-LT-HBARRANTE>
2015-02-24 15:42:04 -0600

    -Se agrega la imagen a la página principal -Se cambia el nombre de la carpeta para seguir el estándar de Spring

--- src/main/java/com/hbv/ciea/WebSecurityConfig.java
+++ src/main/java/com/hbv/ciea/WebSecurityConfig.java
@@ -49,7 +49,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/webjars/**", "/resources/**");
+        web.ignoring().antMatchers("/webjars/**");
     }
 
     @Override
