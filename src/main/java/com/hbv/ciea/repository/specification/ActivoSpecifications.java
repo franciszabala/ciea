7f55be4de33ac287f23a9cbb712a93f8fc435bba
Herman Barrantes <Herman Barrantes@GB-LT-HBARRANTE>
2015-03-09 17:06:59 -0600

    -Se agregó Metamodel de JPA

--- src/main/java/com/hbv/ciea/repository/specification/ActivoSpecifications.java
+++ src/main/java/com/hbv/ciea/repository/specification/ActivoSpecifications.java
@@ -16,6 +16,8 @@
 package com.hbv.ciea.repository.specification;
 
 import com.hbv.ciea.model.Activo;
+import com.hbv.ciea.model.Activo_;
+import com.hbv.ciea.model.Sitio_;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
@@ -26,32 +28,45 @@ import org.springframework.data.jpa.domain.Specification;
  *
  * @author Herman
  * @since 03/03/2015
- * @see http://spring.io/blog/2011/04/26/advanced-spring-data-jpa-specifications-and-querydsl/
- * @see https://blog.42.nl/articles/spring-data-jpa-with-querydsl-repositories-made-easy/
+ * @see
+ * http://spring.io/blog/2011/04/26/advanced-spring-data-jpa-specifications-and-querydsl/
+ * @see
+ * https://blog.42.nl/articles/spring-data-jpa-with-querydsl-repositories-made-easy/
  * @see http://www.altuure.com/2010/09/23/jpa-criteria-api-by-samples-part-i/
- * @see http://stackoverflow.com/questions/7790794/some-basic-questions-on-criteria-from-jpa-2-0
+ * @see
+ * http://stackoverflow.com/questions/7790794/some-basic-questions-on-criteria-from-jpa-2-0
  * @see http://docs.oracle.com/javaee/6/tutorial/doc/gjivm.html
  * @see http://en.wikibooks.org/wiki/Java_Persistence/Criteria
- * 
+ *
  */
 public class ActivoSpecifications {
-    
-    public static Specification<Activo> hasPlaca(final String placa) {
+
+    public static Specification<Activo> isHabilitado() {
         return new Specification<Activo>() {
-            
+
             @Override
             public Predicate toPredicate(Root<Activo> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
-                return cb.like(cb.lower(root.get("placa").as(String.class)), placa.toLowerCase());
+                return cb.isTrue(root.get(Activo_.habilitado));
             }
         };
     }
     
-    public static Specification<Activo> inSitio(final Long sitio) {
+    public static Specification<Activo> hasPlaca(final String placa) {
         return new Specification<Activo>() {
-            
+
             @Override
             public Predicate toPredicate(Root<Activo> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
-                return cb.equal(root.join("sitio").get("id"), sitio);
+                return cb.like(cb.lower(root.get(Activo_.placa)), placa.toLowerCase());
+            }
+        };
+    }
+
+    public static Specification<Activo> inSitio(final Long sitio) {
+        return new Specification<Activo>() {
+
+            @Override
+            public Predicate toPredicate(Root<Activo> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                return cb.equal(root.join(Activo_.sitio).get(Sitio_.id), sitio);
             }
         };
     }
